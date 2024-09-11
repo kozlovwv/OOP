a987cc84d68e7e48a92f6f9cf1bae3ffc22c7961
kozlovvv <kirillkozlov2005@list.ru>
2024-09-11 21:06:10 +0700

    test1

--- Task_1_1_1/src/main/java/ru/nsu/kozlov/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/kozlov/Main.java
@@ -1,10 +1,76 @@
 package ru.nsu.kozlov;
 
 public class Main {
-    public static int sum(int a, int b) {
-        return a + b;
+
+    static int[] heapsort(int[] arr) {
+        int len, index_of_last;
+        len = index_of_last = arr.length;
+        int[] heap = new int[len + 1];
+
+        for (int i = 1; i <= len; i++) {
+            heap[i] = arr[i - 1];
+            siftUp(heap, i);
+        }
+
+        for (int i = 0; i < len; i++) {
+            arr[i] = extractMin(heap, index_of_last);
+            index_of_last--;
+        }
+
+        return arr;
     }
+
+    static void siftUp(int[] heap, int k) {
+        int tmp;
+        while (k > 1) {
+            if (heap[k] < heap[k/2]) {
+                tmp = heap[k];
+                heap[k] = heap[k/2];
+                heap[k/2] = tmp;
+                k = k / 2;
+            }
+            else {
+                break;
+            }
+        }
+    }
+
+    static void siftDown(int[] heap, int N) {
+        int tmp, v;
+        int k = 1;
+
+        while (2*k <= N) {
+            v = 2*k;
+
+            if ((2*k + 1 <= N) && (heap[2*k + 1] < heap[2*k])) {
+                v++;
+            }
+
+            if (heap[v] < heap[k]) {
+                tmp = heap[k];
+                heap[k] = heap[v];
+                heap[v] = tmp;
+
+                k = v;
+            }
+            else {
+                break;
+            }
+        }
+    }
+
+    static int extractMin(int[] heap, int index_of_last) {
+        int min = heap[1];
+        heap[1] = heap[index_of_last];
+        index_of_last--;
+        siftDown(heap, index_of_last);
+        return min;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int el: heapsort(new int[] {5, 4, 3, 2, 1})) {
+            System.out.print(el + " ");
+        }
+        System.out.print('\n');
     }
 }
--- Task_1_1_1/src/test/java/ru/nsu/kozlov/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/kozlov/MainTest.java
@@ -7,6 +7,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTest {
     @Test
     void SampleTest() {
-        assertEquals(3, Main.sum(1, 2));
+        assertArrayEquals(new int[] {1, 2, 3, 4, 5}, Main.heapsort(new int[] {5, 4, 3, 2, 1}));
     }
 }
