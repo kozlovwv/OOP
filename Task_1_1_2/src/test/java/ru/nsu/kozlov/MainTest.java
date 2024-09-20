59db6acbc5507ecaf4e3c5bbe2dd9884cad3b31a
kozlovvv <kirillkozlov2005@list.ru>
2024-09-20 15:04:50 +0700

    tests added

--- Task_1_1_2/src/test/java/ru/nsu/kozlov/MainTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/kozlov/MainTest.java
@@ -1,5 +1,8 @@
 package ru.nsu.kozlov;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -8,9 +11,22 @@ import org.junit.jupiter.api.Test;
 
 public class MainTest {
 
+//    @Test
+//    void testForMain() {
+//
+//    }
+
     @Test
-    void testForMain() {
-        Main.main(null);
-        assertTrue(true);
+    void testPrintScores() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        int plS = 10;
+        int dlS = 5;
+        Main.printScores(plS, dlS);
+
+        String consoleOutput = outputStream.toString();
+        assertEquals("Счет " + plS + ":" + dlS + " в вашу пользу\n", consoleOutput);
+        System.setOut(System.out);
     }
 }
