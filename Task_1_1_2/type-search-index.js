9923909cf81e2e449d5130067fbc7920527e6be8
kozlovwv <kozlovwv@users.noreply.github.com>
2024-09-20 08:05:42 +0000

    Deploying to gh-pages from @ kozlovwv/OOP@65c3b22a683f7edd64934ecec1f95d25fe8b369b 🚀

--- Task_1_1_2/type-search-index.js
+++ Task_1_1_2/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.kozlov","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.kozlov","l":"Card"},{"p":"ru.nsu.kozlov","l":"Deck"},{"p":"ru.nsu.kozlov","l":"Main"},{"p":"ru.nsu.kozlov","l":"Player"}]
