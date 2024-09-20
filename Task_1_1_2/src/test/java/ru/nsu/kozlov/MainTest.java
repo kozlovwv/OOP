d1c7e480cfa8f0217b89100a207682e9b260064a
kozlovvv <kirillkozlov2005@list.ru>
2024-09-20 15:16:10 +0700

    tests added

--- Task_1_1_2/src/test/java/ru/nsu/kozlov/MainTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/kozlov/MainTest.java
@@ -24,9 +24,37 @@ public class MainTest {
         int plS = 10;
         int dlS = 5;
         Main.printScores(plS, dlS);
-
         String consoleOutput = outputStream.toString();
         assertEquals("Счет " + plS + ":" + dlS + " в вашу пользу\n", consoleOutput);
+
+        System.setOut(System.out);
+    }
+
+    @Test
+    void testPrintScores2() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        int plS = 20;
+        int dlS = 20;
+        Main.printScores(plS, dlS);
+        String consoleOutput = outputStream.toString();
+        assertEquals("Счет " + plS + ":" + dlS + "\n", consoleOutput);
+
+        System.setOut(System.out);
+    }
+
+    @Test
+    void testPrintScores3() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        int plS = 10;
+        int dlS = 20;
+        Main.printScores(plS, dlS);
+        String consoleOutput = outputStream.toString();
+        assertEquals("Счет " + plS + ":" + dlS + " в пользу дилера\n", consoleOutput);
+
         System.setOut(System.out);
     }
 }
