5a2bd553e82861e3ecd01d2a0f19d3516a86a684
kozlovvv <kirillkozlov2005@list.ru>
2024-09-20 15:50:43 +0700

    tests added

--- Task_1_1_2/src/test/java/ru/nsu/kozlov/DeckTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/kozlov/DeckTest.java
@@ -8,6 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -35,5 +36,21 @@ public class DeckTest {
         Deck deck = new Deck();
         Card card = deck.getCard();
         assertNotNull(card);
+    }
+
+    @Test
+    void testRemakeDeck() {
+        Deck deck = new Deck();
+        Deck deck2 = new Deck();
+
+        for (int i = 0; i < 52; i++) {
+            deck.getCard();
+            deck2.getCard();
+        }
+
+        deck.remakeDeck();
+        deck2.remakeDeck();
+
+        assertNotEquals(deck, deck2);
     }
 }
