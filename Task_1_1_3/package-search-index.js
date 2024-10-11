ac06dc37abb4170628620789f38cd28b1d73c26c
kozlovwv <kozlovwv@users.noreply.github.com>
2024-10-11 04:21:10 +0000

    Deploying to gh-pages from @ kozlovwv/OOP@ce324a63b65a44e604cb002f0ad76b888d4a0340 🚀

--- Task_1_1_3/package-search-index.js
+++ Task_1_1_3/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"ru.nsu.kozlov"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.nsu.kozlov"}];updateSearchResults();
