395a1b66bbd944a2169f3f75b9347f490d6bb463
Kirill Kozlov <k.kozlov2@g.nsu.ru>
2024-10-18 11:36:47 +0700

    nothing

--- Task_1_2_1/src/test/java/ru/nsu/kozlov/MainTest.java
+++ Task_1_2_1/src/test/java/ru/nsu/kozlov/MainTest.java
@@ -2,7 +2,7 @@ package ru.nsu.kozlov;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MainTest {
     @Test
