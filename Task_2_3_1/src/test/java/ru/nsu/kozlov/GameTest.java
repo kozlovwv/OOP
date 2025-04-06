a770cc606f421ef41c48d4b6a5950e70825cc968
Kirill Kozlov <k.kozlov2@g.nsu.ru>
2025-04-06 23:05:55 +0700

    empty test

---
+++ Task_2_3_1/src/test/java/ru/nsu/kozlov/GameTest.java
@@ -0,0 +1,12 @@
+package ru.nsu.kozlov;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+    @Test
+    void test1() {
+        assertTrue(true);
+    }
+}
