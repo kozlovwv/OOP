ea6a1ce20046abf6458431baa9cbc6a5e9256a68
kozlovwv <kozlovwv@users.noreply.github.com>
2025-05-13 07:25:54 +0000

    Deploying to gh-pages from @ kozlovwv/OOP@8075560931355f523678590779fe496dd89dce37 🚀

---
+++ Task_2_1_2/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"ru.nsu.kozlov","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"ru.nsu.kozlov","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
